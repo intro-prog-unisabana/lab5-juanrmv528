e32d8d22488695dccb919a78d733baa450e43641
juanrmv528e <juanramboy1@gmail.com>
2026-03-17 23:19:50 -0500

    17

--- modules.py
+++ modules.py
@@ -1,1 +1,15 @@
-    
+import os
+import math
+
+# 1
+cwd = os.getcwd()
+print(f"Current working directory: {cwd}")
+
+# 2
+num = int(input("Enter an integer: "))
+log_val = math.log2(num)
+print(f"Log base 2 of {num} is: {log_val}")
+
+# 3
+print(f"Floor: {math.floor(log_val)}")
+print(f"Ceiling: {math.ceil(log_val)}")
